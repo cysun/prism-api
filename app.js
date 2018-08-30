ec1ca5b25a608a8b4c3b3f343eaba41c7bfb189e
Chengyu Sun <csun@calstatela.edu>
2018-08-30 15:53:27 -0700

    Change bodyparser limit
    Hope this would finally do the trick

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const routes = require('./routes');
 require('./lib/config/passport');
 
 app.use(morgan);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
